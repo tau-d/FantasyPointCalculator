c3c8662d04be8ae3c9a1198f0cc502fbc424464a
David Tau <alex.tau518@gmail.com>
2017-06-03 03:32:35 -0400

    Scoreboard parsing
    Added methods to parse weekly and accumulate scoreboard stats for splits
    without a player statistics table (doesn't work for some splits yet bc
    the table formats are different).

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,19 +18,133 @@ import org.jsoup.select.Elements;
 
 
 public class Main {
+	// TODO: get player stats by parsing scoreboards for older seasons/seasons without player stats
+	// TODO: take url for the split (http://lol.gamepedia.com/2017_NA_LCS/Spring_Split), use player stats if the page exists, scoreboards otherwise
+	// TODO: parse column headers and use to cover any stat ordering
+	
+	private static final String[] ORDERED_POSITIONS = {Player.TOP, Player.JGL, Player.MID, Player.ADC, Player.SUP};
+	private static final NumberFormat NUM_FORMATTER = NumberFormat.getInstance(Locale.US);
+	
+	private static class docGetter implements Runnable {
 
-	private static void parseUrlAndAddPlayers(String url, List<Player> players, List<Player> mids, List<Player> adcs, 
-				List<Player> supports, List<Player> tops, List<Player> junglers) {
+		private List<Document> docs;
+		private String url;
+		
+		public docGetter(List<Document> docs, String url) {
+			this.docs = docs;
+			this.url = url;
+		}
+		
+		@Override
+		public void run() {
+			Document doc = null;
+			try {
+				System.out.println(url + " STARTING");
+				doc = Jsoup.connect(url).timeout(60*1000).get();
+				System.out.println(url + " COMPLETE");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			docs.add(doc);
+		}
+		
+	}
+	
+	// METHODS
+	
+	private static void parseAllSplitScoreboards(String url) {
+		Map<Player, Player> storedPlayers = new HashMap<>();
+		
+		List<Document> docs = Collections.synchronizedList(new ArrayList<>(9));
+		List<Thread> threads = new ArrayList<>();
+				
+		for (int i = 1; i <= 9; ++i) {
+			Thread t = new Thread(new docGetter(docs, url + "/Week_" + i));
+			t.start();
+			threads.add(t);			
+		}
+		
+		for (Thread t : threads) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("All docs ready!");
+		
+		for (Document d : docs) {
+			parseOneWeekScoreboard(d, storedPlayers);
+		}
+		
+		List<Player> all_players = new ArrayList<>(storedPlayers.values());
+		saveStats("scoreboard_data\\", all_players);
+	}
+	
+	private static void parseOneWeekScoreboard(Document doc, Map<Player, Player> storedPlayers) {
+		Elements gameTables = doc.getElementsByClass("match-recap");
+		for (Element game : gameTables) {
+			Element tbody = game.child(game.children().size() - 1);
+
+			Element teams = tbody.child(1);
+			String team1 = teams.child(0).ownText();
+			String team2 = teams.child(3).ownText();
+			
+			Element playerStatRows = tbody.child(6).child(0).child(1).child(0);
+			
+			try {
+				// Team 1
+				parseFiveScoreboardRows(playerStatRows, 1, team1, storedPlayers);
+				
+				// Team 2
+				parseFiveScoreboardRows(playerStatRows, 7, team2, storedPlayers);
+			} catch (ParseException e) {
+				e.printStackTrace();
+				System.err.println(team1 + " vs " + team2);
+			}			
+		}
+	}
+	
+	private static void parseFiveScoreboardRows(Element playerStatRows, int startIndex, String teamName, Map<Player, Player> players) throws ParseException {
+		for (int i = startIndex; i < startIndex + 5; ++i) {
+			Element playerRow = playerStatRows.child(i);
+			
+			String playerName = playerRow.child(1).child(0).attr("title");
+			String position = ORDERED_POSITIONS[i - startIndex];
+			int kills = NUM_FORMATTER.parse(playerRow.child(5).ownText()).intValue();
+			int deaths = NUM_FORMATTER.parse(playerRow.child(6).ownText()).intValue();
+			int assists = NUM_FORMATTER.parse(playerRow.child(7).ownText()).intValue();
+			int creepScore = NUM_FORMATTER.parse(playerRow.child(16).ownText()).intValue();
+
+			Player p = new Player(teamName, playerName, position, 1, kills, deaths, assists, creepScore);
+			
+			Player existing = players.get(p);
+			if (existing != null) {
+				combinePlayerStats(existing, p);
+			} else {
+				players.put(p, p);
+			}
+		}
+	}
+	
+	private static void combinePlayerStats(Player main, Player secondary) {
+		main.numGames += secondary.numGames;
+		main.kills += secondary.kills;
+		main.deaths += secondary.deaths;
+		main.assists += secondary.assists;
+		main.creepScore += secondary.creepScore;
+	}
+	
+	private static void parsePlayerStatsAndAddPlayers(String url, List<Player> players) {
 		Document doc = null;
 		try {
-			doc = Jsoup.connect(url).timeout(10*1000).get();
+			System.out.println(url + " STARTING");
+			doc = Jsoup.connect(url).timeout(60*1000).get();
 			
-			Elements links = doc.getElementsByClass("sortable wikitable");
-			Element table = links.get(1);
+			Elements links = doc.getElementsByClass("wikitable");
+			Element table = links.get(links.size() - 1);
 			Element tbody = table.child(0);
 			Elements rows = tbody.getElementsByTag("tr");
-						
-			NumberFormat format = NumberFormat.getInstance(Locale.US);
 			
 			for (int i = 1; i < rows.size(); ++i) {
 				Element row = rows.get(i);
@@ -41,15 +158,16 @@ public class Main {
 								
 				String playerName = row.child(1).ownText();
 				String position = row.child(2).ownText();
-				int numGames = format.parse(row.child(3).ownText()).intValue();
-				int kills = format.parse(row.child(7).ownText()).intValue();
-				int deaths = format.parse(row.child(8).ownText()).intValue();
-				int assists = format.parse(row.child(9).ownText()).intValue();
-				int creepScore = format.parse(row.child(11).ownText()).intValue();
+				int numGames = NUM_FORMATTER.parse(row.child(3).ownText()).intValue();
+				int kills = NUM_FORMATTER.parse(row.child(7).ownText()).intValue();
+				int deaths = NUM_FORMATTER.parse(row.child(8).ownText()).intValue();
+				int assists = NUM_FORMATTER.parse(row.child(9).ownText()).intValue();
+				int creepScore = NUM_FORMATTER.parse(row.child(11).ownText()).intValue();
 								
 				Player p = new Player(team, playerName, position, numGames, kills, deaths, assists, creepScore);
 				players.add(p);
 			}
+			System.out.println(url + " COMPLETE");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -61,7 +179,7 @@ public class Main {
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path + filename + ".csv"))) {
 			bw.write("Team,Player,Position,Games,Kills,Deaths,Assists,CS,Avg Points/Game\n");
 			for (Player p : players) {
-				bw.write(p.toString(",") + "\n");
+				bw.write(p.myToString(",") + "\n");
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -69,31 +187,35 @@ public class Main {
 
 	}
 	
-	public static void main(String[] args) {
-		List<Player> players = new ArrayList<>();
+	private static void calcAndSavePointsFromPlayerStats() {
+		List<Player> all_players = new ArrayList<>();
+		
+		// North America
+		parsePlayerStatsAndAddPlayers("http://lol.gamepedia.com/2017_NA_LCS/Spring_Split/Player_Statistics", all_players);
+		
+		// Europe
+		//parsePlayerStatsAndAddPlayers("http://lol.gamepedia.com/2017_EU_LCS/Spring_Split/Players", all_players);
+		
+		// Korea
+		///parseUrlAndAddPlayers("http://lol.gamepedia.com/2017_LCK/Spring_Split/Players", all_players);
+		
+		// Old format test
+		//parsePlayerStatsAndAddPlayers("http://lol.gamepedia.com/2015_NA_LCS_Spring/Statistics", all_players);
+		
+		saveStats("player_stats_data\\", all_players);
+	}
+	
+	private static void saveStats(String dirPath, List<Player> all_players) {
+		Collections.sort(all_players, (o1, o2) -> (o1.calcAvgPointsPerGame() > o2.calcAvgPointsPerGame()) ? -1 : 1);
+		
 		List<Player> mids = new ArrayList<>();
 		List<Player> adcs = new ArrayList<>();
 		List<Player> supports = new ArrayList<>();
 		List<Player> tops = new ArrayList<>();
 		List<Player> junglers = new ArrayList<>();
 		
-		// North America
-		parseUrlAndAddPlayers("http://lol.gamepedia.com/2017_NA_LCS/Spring_Split/Player_Statistics",
-				players, mids, adcs, supports, tops, junglers);
-		
-		// Europe
-		parseUrlAndAddPlayers("http://lol.gamepedia.com/2017_EU_LCS/Spring_Split/Players",
-				players, mids, adcs, supports, tops, junglers);
-		
-		// Korea
-		/*parseUrlAndAddPlayers("http://lol.gamepedia.com/2017_LCK/Spring_Split/Players",
-				players, mids, adcs, supports, tops, junglers);*/
-		
-	
-		Collections.sort(players, (o1, o2) -> (o1.getAPPG() > o2.getAPPG()) ? -1 : 1);
-		
 		// add to position lists after sort so they are already sorted
-		for (Player p : players) {
+		for (Player p : all_players) {
 			String pos = p.getPos();
 			if (pos.equals(Player.MID)) {
 				mids.add(p);
@@ -108,39 +230,21 @@ public class Main {
 			}
 		}			
 		
-		/*for (Player p : players) {
-			System.out.println(p);
-		}*/
+		writePlayersToCsvFile(dirPath, "all_players", all_players);
+		writePlayersToCsvFile(dirPath, "mid", mids);
+		writePlayersToCsvFile(dirPath, "adc", adcs);
+		writePlayersToCsvFile(dirPath, "sup", supports);
+		writePlayersToCsvFile(dirPath, "top", tops);
+		writePlayersToCsvFile(dirPath, "jgl", junglers);
+	}
+	
+	public static void main(String[] args) {
+		// TODO: take URLs as args?
 		
-		/*System.out.println("\nMIDDLE");
-		for (Player p : mids) {
-			System.out.println(p);
-		}
-		System.out.println("\nADC");
-		for (Player p : adcs) {
-			System.out.println(p);
-		}
-		System.out.println("\nSUPPORT");
-		for (Player p : supports) {
-			System.out.println(p);
-		}
-		System.out.println("\nTOP");
-		for (Player p : tops) {
-			System.out.println(p);
-		}
-		System.out.println("\nJUNGLE");
-		for (Player p : junglers) {
-			System.out.println(p);
-		}*/
-		
-		String path = "data\\";
-				
-		writePlayersToCsvFile(path, "all_players", players);
-		writePlayersToCsvFile(path, "mid", mids);
-		writePlayersToCsvFile(path, "adc", adcs);
-		writePlayersToCsvFile(path, "sup", supports);
-		writePlayersToCsvFile(path, "top", tops);
-		writePlayersToCsvFile(path, "jgl", junglers);
+		//calcAndSavePointsFromPlayerStats();
+		//parseAllSplitScoreboards("http://lol.gamepedia.com/2017_NA_LCS/Spring_Split/Scoreboards");
+		//parseAllSplitScoreboards("http://lol.gamepedia.com/2017_EU_LCS/Spring_Split/Scoreboards");
+		parseAllSplitScoreboards("http://lol.gamepedia.com/2015_NA_LCS_Spring/Scoreboards/Round_Robin");		
 	}
 
 }
--- src/Player.java
+++ src/Player.java
@@ -3,7 +3,7 @@ import java.text.NumberFormat;
 
 public class Player {
 	private static final String SEPARATOR = "\t";
-	private static final NumberFormat FORMATTER = new DecimalFormat("#0.00");
+	private static final NumberFormat DECIMAL_FORMATTER = new DecimalFormat("#0.00");
 	
 	public static final String MID = "MID";
 	public static final String ADC = "ADC";
@@ -19,7 +19,6 @@ public class Player {
 	int deaths;
 	int assists;
 	int creepScore;
-	double avgPointsPerGame;
 	
 	public Player(String team, String playerName, String position, int numGames, int kills, int deaths, int assists, int creepScore) {
 		this.team = team;
@@ -30,31 +29,17 @@ public class Player {
 		this.deaths = deaths;
 		this.assists = assists;
 		this.creepScore = creepScore;
-		avgPointsPerGame = (2 * kills - 0.5 * deaths + 1.5 * assists + 0.01 * creepScore) / numGames;
 	}
 
-	public double getAPPG() {
-		return avgPointsPerGame;
+	public double calcAvgPointsPerGame() {
+		return (2 * kills - 0.5 * deaths + 1.5 * assists + 0.01 * creepScore) / numGames;
 	}
 	
 	public String getPos() {
 		return position;
 	}
 	
-	@Override
-	public String toString() {
-		return 	team + SEPARATOR + 
-				playerName + SEPARATOR + 
-				position + SEPARATOR +
-				numGames + SEPARATOR + 
-				kills + SEPARATOR +
-				deaths + SEPARATOR +
-				assists + SEPARATOR +
-				creepScore + SEPARATOR +  
-				FORMATTER.format(avgPointsPerGame);
-	}
-	
-	public String toString(String mySeparator) {
+	public String myToString(String mySeparator) {
 		return 	team + mySeparator + 
 				playerName + mySeparator + 
 				position + mySeparator +
@@ -63,6 +48,49 @@ public class Player {
 				deaths + mySeparator +
 				assists + mySeparator +
 				creepScore + mySeparator +  
-				FORMATTER.format(avgPointsPerGame);
+				DECIMAL_FORMATTER.format(calcAvgPointsPerGame());
 	}
+	
+	@Override
+	public String toString() {
+		return 	myToString(SEPARATOR);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((playerName == null) ? 0 : playerName.hashCode());
+		result = prime * result + ((position == null) ? 0 : position.hashCode());
+		result = prime * result + ((team == null) ? 0 : team.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Player other = (Player) obj;
+		if (playerName == null) {
+			if (other.playerName != null)
+				return false;
+		} else if (!playerName.equals(other.playerName))
+			return false;
+		if (position == null) {
+			if (other.position != null)
+				return false;
+		} else if (!position.equals(other.position))
+			return false;
+		if (team == null) {
+			if (other.team != null)
+				return false;
+		} else if (!team.equals(other.team))
+			return false;
+		return true;
+	}
+	
 }
